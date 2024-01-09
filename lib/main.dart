7f188b2266b98646dbc476336604587e7a039ec2
hammerandnail <yelionel26@gmail.com>
2024-01-09 14:19:20 +0800

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:zoom_clone/screens/home_screen.dart';
 import 'package:zoom_clone/utils/colors.dart';
 
 import 'screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
